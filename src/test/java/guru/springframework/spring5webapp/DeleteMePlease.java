22a1932b59a7c9786b00a15db734ccf81b33cfab
Tim <timothy.zschernitz@nelnet.net>
2021-10-05 10:27:35 -0500

    Test commit
     - Just trying to see if the gitconfig is retaining the right creds.

---
+++ src/test/java/guru/springframework/spring5webapp/DeleteMePlease.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5webapp;
+
+public class DeleteMePlease {
+
+	public static void goob() {
+		System.out.println("well I'm just here to test git, I tell ya whut.");
+	}
+}
